42bb7ea40428a596f72128ef70ced3b650eb5885
BY <alucard_118@163.com>
2017-03-02 23:03:23 +0800

    lesson4-0302-2

--- lesson4/app.js
+++ lesson4/app.js
@@ -8,6 +8,7 @@ app.use('/',indexRouter);
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
+app.use('/public',express.static(__dirname+'/public'));
 
 app.listen('3000',function () {
 	console.log('App is running at port 3000');
